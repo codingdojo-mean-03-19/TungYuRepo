2815e2e069b876d122e25f4dc97612709f2f6165
Enkbaatar Battulga <tina80925@hotmail.com>
2019-03-23 17:19:48 -0400

    Express Cars and Cats

--- 03-express/01-express.js/01-cars-and-cats/server.js
+++ 03-express/01-express.js/01-cars-and-cats/server.js
@@ -1,40 +1,21 @@
-// var express = require("express");
-
-// var app = express();
-
-// app.get('/', function(request, respond){
-// });
-
-// app.get('/cars', function(request, respond){
-// });
-
-// app.get('/cats', function(request, respond){
-// });
-
-// app.get('/form', function(request, respond){
-// });
-
-// app.use(express.static(__dirname + "/static"));
-
-// app.listen(8000, function(){
-//     console.log("listening on port 8000");
-// })
-
 var express = require("express");
 
 var app = express();
 
-app.get('/', function(request, response) {
+app.get('/', function(request, respond){
 });
-app.get('/cars', function (request, response) {
+
+app.get('/cars', function(request, respond){
 });
-app.get('/cats', function (request, response) {
+
+app.get('/cats', function(request, respond){
 });
-app.get('/form', function (request, response) {
+
+app.get('/form', function(request, respond){
 });
 
 app.use(express.static(__dirname + "/static"));
 
-app.listen(8000, function() {
-	console.log("Listening on port 8000");
+app.listen(8000, function(){
+    console.log("listening on port 8000");
 })
