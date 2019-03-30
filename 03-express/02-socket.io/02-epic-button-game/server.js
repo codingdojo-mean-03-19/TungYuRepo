42880a43cec67e01ed8ac5d146967a8cc89573a0
Enkbaatar Battulga <tina80925@hotmail.com>
2019-03-29 21:03:24 -0400

    Epic Button Game

---
+++ 03-express/02-socket.io/02-epic-button-game/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 8000;
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'client')));
+
+const server = app.listen(port, () => console.log(`listening on port ${ port }`));
+const io = require('socket.io')(server);
+
+let count = 0;
+io.on('connection', socket => {
+    console.log('incoming socket connection');
+
+    socket.on('buttonClicked', function(){
+        // io.emit('numberUpdated', ++count)
+        numberUpdated(++count);
+    });
+
+    socket.on('reset', function(){
+        // count = 0;
+        // io.emit('numberUpdated', count);
+        numberUpdated(count = 0);
+    });
+
+    socket.emit('numberUpdated', count);
+}); 
+ function numberUpdated(number){
+     io.emit('numberUpdated', number);
+ }
