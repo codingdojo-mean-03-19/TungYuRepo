bd2f012c78d1ba6cacfd8a8fc5fe88af6bf4579e
Enkbaatar Battulga <tina80925@hotmail.com>
2019-04-22 18:08:28 -0400

    Restful Tasks Interactive

---
+++ 05-angular/04-dom-manipulation-events/02-restful-tasks-interactive/server/controllers/task.controller.js
@@ -0,0 +1,41 @@
+const Task = require('mongoose').model('Task');
+const { Http } = require('@status/codes');
+
+
+module.exports = {
+  //retrieve all data
+  index(_req, res){
+    Task.find({})
+    .then(result => res.json(result))
+    .catch(error => res.status(Http.InternalServerError).json(error))
+  },
+  //create new data
+  create(req, res){
+    Task.create(req.body)
+    .then(result => res.json(task))
+    .catch(error => {
+      const errors = Object.keys(error.errors).map(key => error.errors[key].message);
+      res.status(Http.UnprocessableEntity).json(errors);
+    })
+  },
+  //show data
+  show(req, res){
+    const { Task_id: TaskId } = req.params;
+    Task.findById(TaskId)
+    .then(result => res.json(result))
+    .catch(error => status(Http.UnavailableForLegalReasons).json(error));
+  },
+  //update data
+  update(req, res){
+    const { task_id: taskId } = req.params;
+    Task.findByIdAndUpdate(taskId, req.body, { new: true })
+    .then(result => res.json(result))
+    .catch(error => status(Http.UnprocessableEntity).json(error));
+  },
+  //delete data
+  destroy(_req, res){
+    Task.findByIdAndDelete(taskId)
+    .then(result => res.json(result))
+    .catch(error => res.status(Http.ResetContent).json(error));
+  },
+}
---
+++ 05-angular/04-dom-manipulation-events/02-restful-tasks-interactive/server/models/task.model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const TaskSchema = new Schema({
+  title: {
+    type: String,
+    trim: true,
+    required: [true, 'Title is required'],
+    minlength: [2, 'More than 1 char needed']
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+    trim: true
+  }
+} , {
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  }
+});
+
+module.exports = mongoose.model('Task', TaskSchema);
